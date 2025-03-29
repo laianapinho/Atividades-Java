73c289ef8148d23736fd9977a89b2567461a06e0
laianapinho <laiana.cavalcante15@gmail.com>
2025-03-28 20:54:13 -0400

    Adicionando meu arquivo ao GitHub

---
+++ Coelho.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Coelho {
+    public static void main(String[] args) {
+    System.out.println("(\\ (\\");
+    System.out.println("(='.')");
+    System.out.println("((^)(^)");
+}
+}
