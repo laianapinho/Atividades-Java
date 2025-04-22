e3274252f750cb950d5315c3e3a03abf922bbc73
laianapinho <laiana.cavalcante15@gmail.com>
2025-04-21 21:17:52 -0400

    Adiciona novo

---
+++ Quadrado.java
@@ -0,0 +1,14 @@
+package br.edu.icomp.ufam.lab_heranca;
+
+public class Quadrado extends Retangulo {
+
+    public Quadrado(int posX, int posY, double lado) {
+        super(posX, posY, lado, lado); 
+    }
+
+    @Override
+    public String toString() {
+        return "Quadrado na posição (" + this.posX + ", " + this.posY + ") com lado de " + this.largura +
+               "cm (área=" + getArea() + "cm2, perímetro=" + getPerimetro() + "cm)";
+    }
+}
